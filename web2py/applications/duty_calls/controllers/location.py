f51a75a964dfef65650aec9f7c3ea2ddcff2e9e5
Austen Smith <smitau01@luther.edu>
2013-05-09 09:06:42 -0500

    Starting to develop location controler and basic view

--- web2py/applications/duty_calls/controllers/location.py
+++ web2py/applications/duty_calls/controllers/location.py
@@ -3,10 +3,17 @@ import util
 def display():
     args = request.args
     location_name = args[0]
+    db = current.db
 
     # TODO: I'm using like() instead of == to provide case-insensitive lookup.
     # PostgreSQL may behave differently than MySQL and SQLite
     location = util.getLocationFromName(location_name)
+    onDuty = util.getCurrentPersonsOnDuty(location)
+    onDuty_string = ""
+    for name in onDuty:
+        onDuty_string = onDuty_string + name + ", "
+
+    contacts = None
 
     twilio_number_str = util.getTwilioNumber(location)
 
@@ -15,4 +22,6 @@ def display():
     print ("CurrentForwarding: ", util.getCurrentForwardingDestinations(location))
     print("Current on Cal: ", util.getCurrentPersonsOnDuty(location))
 
-    return dict(location=location)
+    return dict(location=location, twilio_number_str=twilio_number_str, onDuty=onDuty)
+
+    
