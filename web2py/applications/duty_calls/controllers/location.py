2c5346e8c545f891d4deea055eccb4f866d3c3a9
Austen Smith <smitau01@luther.edu>
2013-05-10 07:54:28 -0500

    basic view work

--- web2py/applications/duty_calls/controllers/location.py
+++ web2py/applications/duty_calls/controllers/location.py
@@ -13,7 +13,28 @@ def display():
     for name in onDuty:
         onDuty_string = onDuty_string + name + ", "
 
-    contacts = None
+    #TODO make sure all location names are stored as all lower case
+
+    q = db.auth_user.locations.contains(location.id)
+    contacts = db(q).select()
+
+    #TODO get ahd id dynamically
+
+    ahd_group_id = 2
+
+    q = ((db.auth_membership.group_id == ahd_group_id) & 
+        (db.auth_membership.user_id == db.auth_user.id) & 
+        (db.auth_user.locations.contains(location['id'])))
+    ahd_list = db(q).select()
+
+    ahd_string = ""
+    for row in ahd_list:
+        ahd_string = ahd_string + row.auth_user.first_name + " " + row.auth_user.last_name + ", "
+
+    print("CONTACTS: ")
+
+    for row in contacts:
+        print(row.first_name)
 
     twilio_number_str = util.getTwilioNumber(location)
 
@@ -22,6 +43,6 @@ def display():
     print ("CurrentForwarding: ", util.getCurrentForwardingDestinations(location))
     print("Current on Cal: ", util.getCurrentPersonsOnDuty(location))
 
-    return dict(location=location, twilio_number_str=twilio_number_str, onDuty=onDuty)
+    return dict(location=location, twilio_number_str=twilio_number_str, onDuty=onDuty, ahd_string = ahd_string)
 
     
