690da4a1c0c817309495ea11a55b69fc1903c8dd
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-05-09 19:21:58 -0500

    add location-specific update controller

--- web2py/applications/duty_calls/controllers/update.py
+++ web2py/applications/duty_calls/controllers/update.py
@@ -1,18 +1,5 @@
 import util
 
-def test():
-    args = request.args
-    location_name = args[0]
-
-    location = util.getLocationFromName(location_name)
-    #print "On duty: " + str(util.getCurrentPersonsOnDuty(location))
-    #print "current destinations: " + str(util.getCurrentForwardingDestinations(location))
-    #util.update(location)
-    #print "updated destinations: " + str(util.getCurrentForwardingDestinations(location))
-    #print ""
-    #util.logError("Test message", location)
-
-
 def all():
     q = db.locations.id > 0
     locations = db(q).select()
@@ -21,3 +8,14 @@ def all():
         print "Updating " + loc.location_name
         print ""
         util.update(loc)
+        
+
+def location():
+    args = request.args
+    location_name = args[0]
+    
+    location = util.getLocationFromName(location_name)
+
+    print "Updating " + loc.location_name
+    print ""
+    util.update(location)
