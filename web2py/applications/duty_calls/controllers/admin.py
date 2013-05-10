f3174a92eb9fee2805dec31f87ec496bf1870c46
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-05-09 21:00:16 -0500

    add controller and view to display users

--- web2py/applications/duty_calls/controllers/admin.py
+++ web2py/applications/duty_calls/controllers/admin.py
@@ -4,7 +4,7 @@ import util
 def locations():
     args = request.args
 
-    ## if there is a location specified (e.g. ...location/brandt) ##
+    ## if there is a location specified (e.g. ...locations/brandt) ##
     if len(args) > 0: 
         location_name = args[0]
         location = util.getLocationFromName(location_name)
@@ -16,4 +16,26 @@ def locations():
         locations = db(q).select()
 
     return dict(locations=locations)
+
+
+def users():
+    args = request.args
+        
+    ## if there is a location specified to show users from (e.g. users/brandt) ##
+    if len(args) > 0:
+        location_name = args[0]
+        location = util.getLocationFromName(location_name)
+
+        # TODO figure out how to just get users for specific location
+        q = (db.auth_user.id > 0) & (db.auth_user.locations.contains(location))
+    
+    ## no location specified; show all users ##
+    else:
+        location = None
+        q = db.auth_user.id > 0
+
+    
+    users = db(q).select()
+    return dict(users=users, location=location)
+
 
