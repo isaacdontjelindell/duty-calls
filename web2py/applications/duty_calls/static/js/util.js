7acc30831133ad69077acd7b9923d56df1fffeb9
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-05-11 16:54:12 -0500

    javascript to get the ids of users to be added; still need to generate the post request to /%location_name%/add

--- web2py/applications/duty_calls/static/js/util.js
+++ web2py/applications/duty_calls/static/js/util.js
@@ -1,52 +1,31 @@
-function populateAllUsers(loc_name) {
-    $.getJSON('/duty_calls/json/users.json', function(data) {
-        var items = [];
-        var list = $("#add-user-list")
-        
-        items.push("<form name='add_users' method=POST action='" + loc_name + "/add'>");
-        items.push('<table>');
-        items.push('<tr>');
-        items.push('<td>' + "Select" + '</td>');
-        items.push('<td>' + "User ID" + '</td>');
-        items.push('<td>' + "Name" + '</td>');
-        items.push('<td>' + "Phone" + '</td>');
-        items.push('<td>' + "Email" + '</td>');
-        items.push('</tr>');
-        $.each(data['users'], function(uid, user_data) {
-            items.push('<tr>');
-            items.push('<td>' + "<input type=checkbox name='add_ids' value='" + uid + "'>" + '</td>');
-            items.push('<td>' + uid + '</td>');
-            items.push('<td>' + user_data['name'] + '</td>');
-            items.push('<td>' + user_data['phone'] + '</td>');
-            items.push('<td>' + user_data['email'] + '</td>');
-            items.push('</tr>');
-        });
-        items.push('</table>')
-        items.push("<input type=submit value='Add Selected Users'>")
-        items.push('</form>')
 
-        var html = '';
-        for(i=0; i<items.length; i++) {
-            //list.append(items[i]);
-            html = html + items[i];
-        }
-        list.html(html);
-    });
-}
-
-var user_table;   
+var add_user_table;   
 function toggleAddUser(loc_name) {
         
     $('#add-user-div').toggle();
     
-    if (!user_table) {
-        user_table = $('#add-user-list').dataTable( {
+    if (!add_user_table) {
+        add_user_table = $('#add-user-list').dataTable( {
             "bProcessing": true,
             "sAjaxSource": '/duty_calls/json/users.json',
+            "aoColumnDefs": [
+                { "bSearchable": false, "bVisible": false, "aTargets": [ 3 ] }
+            ]
         });
 
         $('#add-user-list tbody').on( 'click', 'tr', function () {
             $(this).toggleClass('row_selected');
         } );
     }
+}
+
+function getAddUserTableSelectedUserIds() {
+    selected_rows = add_user_table.$('tr.row_selected');
+    user_ids = []
+    for(var i=0; i<selected_rows.length; i++) {
+        var position = add_user_table.fnGetPosition(selected_rows[i]);
+        var uid = add_user_table.fnGetData(position)[3];
+        user_ids.push(uid);       
+    }
+    return user_ids
 }
